a680ae3499401821eefb6f176487aac0198199a3
Manuel Reithuber <git@manuel.reithuber.net>
2019-05-03 16:38:35 +0200

    web: made linkBuilder call-by-value

--- web/url.go
+++ web/url.go
@@ -6,24 +6,23 @@ type linkBuilder struct {
 	URL url.URL
 }
 
-func (l *linkBuilder) WithParam(key string, values ...string) *linkBuilder {
-	l.WithParamArray(key, values)
-	return l
+func (l linkBuilder) WithParam(key string, values ...string) linkBuilder {
+	return l.WithParamArray(key, values)
 }
 
 // WithParamArray -- flavor of SetParam that accepts an array (for use in templates)
-func (l *linkBuilder) WithParamArray(key string, values []string) *linkBuilder {
+func (l linkBuilder) WithParamArray(key string, values []string) linkBuilder {
 	var query = l.URL.Query()
 	query[key] = values
 	l.URL.RawQuery = query.Encode()
 	return l
 }
 
-func (l *linkBuilder) WithPath(path string) *linkBuilder {
+func (l linkBuilder) WithPath(path string) linkBuilder {
 	l.URL.Path = path
 	return l
 }
 
-func (l *linkBuilder) String() string {
+func (l linkBuilder) String() string {
 	return l.URL.String()
 }
