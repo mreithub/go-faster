f850900a94a2dd89142f72afdae4390d9e62f420
Manuel Reithuber <git@manuel.reithuber.net>
2019-09-18 00:47:37 +0200

    added new struct internal.Tree (maps paths to indexes in - soon to be created - data arrays)

---
+++ faster/internal/tree.go
@@ -0,0 +1,96 @@
+package internal
+
+// Tree -- assigns sequential indexes to a key hierarchy
+type Tree struct {
+	curIndex int
+	root     *TreeNode
+}
+
+type TreeNode struct {
+	Index    int
+	Children map[string]*TreeNode
+}
+
+func (t *Tree) nextIndex() int {
+	var rc = t.curIndex
+	t.curIndex++
+	return rc
+}
+
+// Clone -- create a deep copy of this Tree object
+//
+// TODO there is potential for optimizations here (e.g. `make([]treeNode, t.curIndex+1` and using that )
+func (t *Tree) Clone() *TreeNode {
+	var rc *TreeNode
+	if t.root != nil {
+		rc = t.root.cloneRec()
+	}
+	return rc
+}
+
+// GetIndex -- returns the sequential index assigned to the given path
+// (will create nodes recursively)
+func (t *Tree) GetIndex(path ...string) int {
+	if t.root == nil {
+		t.root = &TreeNode{
+			Index: t.nextIndex(), // 0
+		}
+	}
+
+	var curNode = t.root
+	for len(path) > 0 {
+		if curNode.Children == nil {
+			curNode.Children = make(map[string]*TreeNode)
+		}
+
+		var child *TreeNode
+		var ok bool
+		if child, ok = curNode.Children[path[0]]; !ok {
+			child = &TreeNode{
+				Index: t.nextIndex(),
+			}
+			curNode.Children[path[0]] = child
+		}
+
+		curNode = child
+		path = path[1:]
+	}
+	return curNode.Index
+}
+
+// Reset -- removes all nodes and resets the sequential index to 0
+func (t *Tree) Reset() {
+	t.root = nil
+	t.curIndex = 0
+}
+
+func (n *TreeNode) cloneRec() *TreeNode {
+	var rc = TreeNode{
+		Index:    n.Index,
+		Children: make(map[string]*TreeNode, len(n.Children)),
+	}
+
+	for key, child := range n.Children {
+		rc.Children[key] = child.cloneRec()
+	}
+	return &rc
+}
+
+// GetIndex -- returns the index of the given path (if found, -1 otherwise)
+func (n *TreeNode) GetIndex(path ...string) int {
+	if len(path) == 0 {
+		return n.Index
+	}
+	if child, ok := n.Children[path[0]]; ok {
+		return child.GetIndex(path[1:]...)
+	}
+	return -1
+}
+
+func (n *TreeNode) Keys() []string {
+	var rc = make([]string, 0, len(n.Children))
+	for k := range n.Children {
+		rc = append(rc, k)
+	}
+	return rc
+}
---
+++ faster/internal/tree_test.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestTree -- creates a tree and checks if the expected indexes get returned
+// (indexes are assigned in the order of the nodes' creation)
+func TestTree(t *testing.T) {
+	var tree Tree
+
+	assert.Equal(t, 3, tree.GetIndex("_faster", "key", "info.json")) // 1 2 3
+	assert.Equal(t, 5, tree.GetIndex("http", "GET /robots.txt"))     // 4 5
+	assert.Equal(t, 6, tree.GetIndex("http", "GET /favicon.ico"))    // 4 6
+	assert.Equal(t, 7, tree.GetIndex("_faster", "key", "foobar"))    // 1 2 7
+	assert.Equal(t, 8, tree.GetIndex("https"))
+
+	assert.Equal(t, 0, tree.GetIndex())
+	assert.Equal(t, 1, tree.GetIndex("_faster"))
+	assert.Equal(t, 2, tree.GetIndex("_faster", "key"))
+	assert.Equal(t, 4, tree.GetIndex("http"))
+}
